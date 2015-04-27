e8f1ebff525709e005a01ce7414e63d17b076057
Miguel Angel Quinones <m.quinones.garcia@gmail.com>
2015-04-27 22:53:47 +0100

    Moves Demo project on level inside root, so Carthage may find the correct project and target to build
    This fixes #1

--- Source/SwiftAnimations.h
+++ Source/SwiftAnimations.h
@@ -11,6 +11,3 @@ FOUNDATION_EXPORT double SwiftAnimationsVersionNumber;
 //! Project version string for SwiftAnimations.
 FOUNDATION_EXPORT const unsigned char SwiftAnimationsVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <SwiftAnimations/PublicHeader.h>
-
-

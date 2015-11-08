f15ab71cbbfa84c16afbd36a69394edd7c9f7c56
Miguel Angel Quinones Garcia <m.quinones.garcia@gmail.com>
2015-11-08 22:03:32 +0000

    Add test stubs

---
+++ Tests/SwiftAnimationsTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftAnimationsTests.swift
+//  SwiftAnimations
+//
+//  Created by Miguel on 08/11/2015.
+//  Copyright © 2015 miqu. All rights reserved.
+//
+
+import XCTest
+
+class Test : XCTestCase {
+    
+    func testDummy() {
+        // Test stub (verifies test target works)
+    }
+}

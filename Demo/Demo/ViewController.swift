e9102df863620a3d8500892a2bf16d9b633841bc
Miguel Angel Quinones Garcia <miguel@miqu.me>
2017-10-06 22:17:56 +0100

    [UPDATE] Demo project to Swift 4.0

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -8,32 +8,32 @@ import SwiftAnimations
 
 class ViewController: UIViewController {
 
-    override func viewDidAppear(animated: Bool) {
+    override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
     }
 
     @IBAction func startAnimations() {
-        let reset: Void->Void = {
-            self.red.transform = CGAffineTransformIdentity
-            self.green.transform = CGAffineTransformIdentity
-            self.yellow.transform = CGAffineTransformIdentity
-            self.blue.transform = CGAffineTransformIdentity
+        let reset: () -> Void = {
+            self.red.transform = CGAffineTransform.identity
+            self.green.transform = CGAffineTransform.identity
+            self.yellow.transform = CGAffineTransform.identity
+            self.blue.transform = CGAffineTransform.identity
         }
         
         animate {
-            self.red.transform = CGAffineTransformMakeRotation(CGFloat(M_PI_2))
+            self.red.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
         }.thenAnimate {
-            self.green.transform = CGAffineTransformMakeRotation(-CGFloat(M_PI_2))
+            self.green.transform = CGAffineTransform(rotationAngle: CGFloat(-Double.pi))
         }.afterDelay(1)
          .thenAnimate {
-            self.blue.transform = CGAffineTransformMakeScale(1.5, 1)
-        }.withOptions(.CurveLinear).withDuration(1).thenAnimate {
-            self.yellow.transform = CGAffineTransformMakeScale(1, 1.5)
-        }.withOptions(.CurveEaseIn)
+            self.blue.transform = CGAffineTransform(scaleX: 1.5, y: 1)
+        }.withOptions(.curveLinear).withDuration(1).thenAnimate {
+            self.yellow.transform = CGAffineTransform(scaleX: 1, y: 1.5)
+        }.withOptions(.curveEaseIn)
          
          .thenAnimate {
-            let scale = CGAffineTransformMakeScale(0.5, 0.5)
+            let scale = CGAffineTransform(scaleX: 0.5, y: 0.5)
             self.red.transform = scale
             self.green.transform = scale
             self.blue.transform = scale
@@ -43,8 +43,8 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBOutlet private var red: UIView!
-    @IBOutlet private var green: UIView!
-    @IBOutlet private var blue: UIView!
-    @IBOutlet private var yellow: UIView!
+    @IBOutlet fileprivate var red: UIView!
+    @IBOutlet fileprivate var green: UIView!
+    @IBOutlet fileprivate var blue: UIView!
+    @IBOutlet fileprivate var yellow: UIView!
 }
